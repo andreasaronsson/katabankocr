fc6cbd80f13e85ec6c14b93031d51923c71e7e7a
Andreas Aronsson <aron@aron.nu>
2016-06-12 23:06:06 +0200

    Simplify

--- src/main/java/aron/nu/katabankocr/Main.java
+++ src/main/java/aron/nu/katabankocr/Main.java
@@ -10,7 +10,6 @@ import static javaslang.collection.Array.collector;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,57 +18,67 @@ import javaslang.Tuple;
 import javaslang.Tuple3;
 import javaslang.collection.Array;
 import javaslang.collection.CharSeq;
+import javaslang.control.Try;
 
+/**
+ * Entry point
+ */
 public class Main {
     static final Logger log = LoggerFactory.getLogger(Main.class);
 
     private Main() {
     }
 
+    /**
+     * main
+     * 
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
-        log.info("BEGIN");
-        Path path = get("./src/test/resources/allnumbers");
-        Array<String> allRows = lines(path).collect(collector());
-        CharSeq rowOne = CharSeq.of(allRows.get(0));
-        CharSeq rowTwo = CharSeq.of(allRows.get(1));
-        CharSeq rowThree = CharSeq.of(allRows.get(2));
-        CharSeq rowFour = CharSeq.of(allRows.get(3));
-        log.info(rowOne.mkString());
-        log.info(rowTwo.mkString());
-        log.info(rowThree.mkString());
-        log.info(rowFour.mkString());
-        log.info("END");
-        Bars firstPosition = findBars(rowOne, rowTwo, rowThree, 1, Tuple.of(0, 1, 2), Tuple.of(0, 1, 2));
-        Bars secondPosition = findBars(rowOne, rowTwo, rowThree, 4, Tuple.of(3, 4, 5), Tuple.of(3, 4, 5));
-        Bars thirdPosition = findBars(rowOne, rowTwo, rowThree, 7, Tuple.of(6, 7, 8), Tuple.of(6, 7, 8));
-        Bars fourthPosition = findBars(rowOne, rowTwo, rowThree, 10, Tuple.of(9, 10, 11), Tuple.of(9, 10, 11));
-        Bars fifthPosition = findBars(rowOne, rowTwo, rowThree, 13, Tuple.of(12, 13, 14), Tuple.of(12, 13, 14));
-        Bars sixthPosition = findBars(rowOne, rowTwo, rowThree, 16, Tuple.of(15, 16, 17), Tuple.of(15, 16, 17));
-        Bars seventhPosition = findBars(rowOne, rowTwo, rowThree, 19, Tuple.of(18, 19, 20), Tuple.of(18, 19, 20));
-        Bars eighthPosition = findBars(rowOne, rowTwo, rowThree, 22, Tuple.of(21, 22, 23), Tuple.of(21, 22, 23));
-        Bars ninthPosition = findBars(rowOne, rowTwo, rowThree, 25, Tuple.of(24, 25, 26), Tuple.of(24, 25, 26));
-        Array<String> result = Array.of(barMatcher(firstPosition), barMatcher(secondPosition),
-                barMatcher(thirdPosition), barMatcher(fourthPosition), barMatcher(fifthPosition),
-                barMatcher(sixthPosition), barMatcher(seventhPosition), barMatcher(eighthPosition),
-                barMatcher(ninthPosition));
-        log.info("Here {}", Arrays.toString(result.toJavaArray()));
+        String pathName = "./src/test/resources/123456789";
+        Path path = get(pathName);
+        String result = readResultFor(path);
+        log.info("Result {} correct {}", result, path.getFileName());
     }
 
-    private static Bars findBars(CharSeq one, CharSeq two, CharSeq three, int first,
+    private static String readResultFor(Path pathName) {
+        Tuple3<CharSeq, CharSeq, CharSeq> row = readRowsFromFile(pathName);
+        Bars firstPosition = findBars(row, 1, Tuple.of(0, 1, 2), Tuple.of(0, 1, 2));
+        Bars secondPosition = findBars(row, 4, Tuple.of(3, 4, 5), Tuple.of(3, 4, 5));
+        Bars thirdPosition = findBars(row, 7, Tuple.of(6, 7, 8), Tuple.of(6, 7, 8));
+        Bars fourthPosition = findBars(row, 10, Tuple.of(9, 10, 11), Tuple.of(9, 10, 11));
+        Bars fifthPosition = findBars(row, 13, Tuple.of(12, 13, 14), Tuple.of(12, 13, 14));
+        Bars sixthPosition = findBars(row, 16, Tuple.of(15, 16, 17), Tuple.of(15, 16, 17));
+        Bars seventhPosition = findBars(row, 19, Tuple.of(18, 19, 20), Tuple.of(18, 19, 20));
+        Bars eighthPosition = findBars(row, 22, Tuple.of(21, 22, 23), Tuple.of(21, 22, 23));
+        Bars ninthPosition = findBars(row, 25, Tuple.of(24, 25, 26), Tuple.of(24, 25, 26));
+        return barMatcher(firstPosition) + barMatcher(secondPosition) + barMatcher(thirdPosition)
+                + barMatcher(fourthPosition) + barMatcher(fifthPosition) +
+                barMatcher(sixthPosition) + barMatcher(seventhPosition) + barMatcher(eighthPosition)
+                + barMatcher(ninthPosition);
+    }
+
+    private static Tuple3<CharSeq, CharSeq, CharSeq> readRowsFromFile(Path pathName) {
+        Array<String> allRows = Try.of(() -> lines(pathName).collect(collector())).get();
+        return Tuple.of(CharSeq.of(allRows.get(0)), CharSeq.of(allRows.get(1)), CharSeq.of(allRows.get(2)));
+    }
+
+    private static Bars findBars(Tuple3<CharSeq, CharSeq, CharSeq> row, int first,
             Tuple3<Integer, Integer, Integer> second, Tuple3<Integer, Integer, Integer> third) {
-        boolean top = Match(one.get(first)).of(
+        boolean top = Match(row._1.get(first)).of(
                 Case($('_'), true), Case($(), false));
-        boolean upLeft = Match(two.get(second._1)).of(
+        boolean upLeft = Match(row._2.get(second._1)).of(
                 Case($('|'), true), Case($(), false));
-        boolean middle = Match(two.get(second._2)).of(
+        boolean middle = Match(row._2.get(second._2)).of(
                 Case($('_'), true), Case($(), false));
-        boolean upRight = Match(two.get(second._3)).of(
+        boolean upRight = Match(row._2.get(second._3)).of(
                 Case($('|'), true), Case($(), false));
-        boolean downLeft = Match(three.get(third._1)).of(
+        boolean downLeft = Match(row._3.get(third._1)).of(
                 Case($('|'), true), Case($(), false));
-        boolean bottom = Match(three.get(third._2)).of(
+        boolean bottom = Match(row._3.get(third._2)).of(
                 Case($('_'), true), Case($(), false));
-        boolean downRight = Match(three.get(third._3)).of(
+        boolean downRight = Match(row._3.get(third._3)).of(
                 Case($('|'), true), Case($(), false));
         return new Bars(top, upLeft, middle, upRight, downLeft, bottom, downRight);
     }
